0f32f880026f1658d5791b88ce0fdb79f5d12ce8
Michaël Petrov <michael@redhat.com>
2022-01-31 10:21:03 +0100

    Matching rules dictionary

--- qyaml.py
+++ qyaml.py
@@ -10,8 +10,10 @@ See README.md for more examples.
 """
 
 import sys
+from types import NoneType
 import yaml
 import re
+from typing import Dict, Generator, Any, Tuple
 
 
 def qyaml(docs, queries):
@@ -21,6 +23,71 @@ def qyaml(docs, queries):
             for ok, value in do_query(doc, query):
                 (result if ok else errors).append(value)
     return result, errors if queries and (result or errors) else [(False, queries)]
+
+
+def matchfunc(doc, query):
+    yield (True, doc) if re.fullmatch(query, doc) else (False, query)
+
+
+def eq(doc, query):
+    yield (True, doc) if query == doc else (False, query)
+
+
+def dict_str(doc, query):
+    keys = filter(lambda k: re.fullmatch(query, k), doc.keys())
+    if keys:
+        yield from ((True, {k: doc[k]}) for k in keys)
+    else:
+        yield (False, query)
+
+
+def dict_bool(doc, query):
+    if query:
+        for k, v in doc.items():
+            yield (True, {k: v})
+    else:
+        for k in doc.keys():
+            yield (True, k)
+
+
+def list_str(doc, query):
+    found = False
+    for d in doc:
+        for ok, x in do_query(d, query):
+            if ok:
+                yield (True, x)
+                found = True
+    if not found:
+        yield (False, query)
+
+
+def list_list(doc, query):
+    for d in doc:
+        result = []
+        for q in query:
+            for ok, x in do_query(d, q):
+                if ok:
+                    result.append(x)
+        if len(result):
+            yield (True, result)
+
+
+def for_q_in_query(doc, query):
+    for q in query:
+        yield from do_query(doc, q)
+
+
+def dict_dict(doc, query):
+    result = {}
+    for k, v in query.items():
+        for ok, x in do_query(doc.get(k), v):
+            if ok:
+                if k in result:
+                    result[k].append(x)
+                else:
+                    result[k] = [x]
+    if len(result):
+        yield (True, result)
 
 
 def dok_list(doc, query):
@@ -36,96 +103,60 @@ def dok(doc, query):
     yield from (dok_list if type(query) == list else dok_scalar)(doc, query)
 
 
-def match(doc, query):
-    return re.fullmatch(query, doc)
+def list_dict(doc, query):
+    f = {}
+    for k, v in query.items():
+        if type(k) == bool:
+            f[k] = v
+            if (not k) in f:
+                break
+    i = 0
+    for d in doc:
+        if True in f and not all(dok(d, f[True])) or False in f and any(dok(d, f[False])):
+            continue
+        only_bool = True
+        for k, v in query.items():
+            if type(k) == bool:
+                continue
+            only_bool = False
+            if type(k) == int and k == i:
+                yield from ((True, x) for ok, x in do_query(d, v) if ok)
+        if only_bool:
+            yield (True, d)
+        else:
+            yield from do_query(d, query)
+        i += 1
+
+
+def x_index(doc, query):
+    yield (True, doc[query]) if 0 <= query < len(doc) else (False, query)
+
+
+def x_key(doc, query):
+    yield (True, {query: doc[query]}) if query in doc else (False, query)
+
+
+MATCHING_RULES: Dict[str, Dict[str, Generator[Tuple[bool, Any], NoneType, NoneType]]] = {
+    None: {None: eq},
+    str: {str: matchfunc, int: x_index, list: for_q_in_query},
+    int: {int: eq, float: eq},
+    float: {int: eq, float: eq},
+    bool: {bool: eq},
+    list: {str: list_str, int: x_index, float: x_index, list: list_list, dict: list_dict},
+    dict: {str: dict_str, int: x_key, float: x_key,
+           bool: dict_bool, list: for_q_in_query, dict: dict_dict}
+}
 
 
 def do_query(doc, query):
-    td, tq, err = type(doc), type(query), (False, query)
-    if doc is None and query is not None:
-        yield err
-    elif tq == str and td == str:
-        yield (True, doc) if match(doc, query) else err
-    elif tq == bool and td == bool or tq in [int, float] and td in [int, float]:
-        yield (True, doc) if query == doc else err
-    elif td == dict and tq == str:
-        keys = filter(lambda k: match(k, query), doc.keys())
-        for k in keys:
-            yield (True, {k: doc[k]})
-        if not keys:
-            yield err
-    elif td == dict and tq in [int, float]:
-        yield (True, {query: doc[query]}) if query in doc else err
-    elif td == dict and tq == bool:
-        if query:
-            for k, v in doc.items():
-                yield (True, {k: v})
-        else:
-            for k in doc.keys():
-                yield (True, k)
-    elif td == list and tq == str:
-        found = False
-        for d in doc:
-            for ok, x in do_query(d, query):
-                if ok:
-                    yield (True, x)
-                    found = True
-        if not found:
-            yield err
-    elif td == dict and tq in [int, float]:
-        yield (True, doc[query]) if query in doc else err
-    elif td in [list, str] and tq in [int, float]:
-        yield (True, doc[query]) if 0 <= query < len(doc) else err
-    elif td == list and tq == list:
-        for d in doc:
-            result = []
-            for q in query:
-                for ok, x in do_query(d, q):
-                    if ok:
-                        result.append(x)
-            if len(result):
-                yield (True, result)
-    elif td in [dict, str] and tq == list:
-        for q in query:
-            yield from do_query(doc, q)
-    elif td == dict and tq == dict:
-        result = {}
-        for k, v in query.items():
-            for ok, x in do_query(doc.get(k), v):
-                if ok:
-                    if k in result:
-                        result[k].append(x)
-                    else:
-                        result[k] = [x]
-        if len(result):
-            yield (True, result)
-    elif td == list and tq == dict:
-        f = {}
-        for k, v in query.items():
-            if type(k) == bool:
-                f[k] = v
-                if (not k) in f:
-                    break
-        i = 0
-        for d in doc:
-            if True in f and not all(dok(d, f[True])) or False in f and any(dok(d, f[False])):
-                continue
-            only_bool = True
-            for k, v in query.items():
-                if type(k) == bool:
-                    continue
-                only_bool = False
-                if type(k) == int and k == i:
-                    for ok, x in do_query(d, v):
-                        if ok:
-                            yield (True, x)
-            if only_bool:
-                yield (True, d)
-            else:
-                yield from do_query(d, query)
-            i += 1
+    rule = MATCHING_RULES.get(type(doc))
+    while rule is not None:
+        rule = rule.get(type(query))
+        if rule is not None:
+            yield from rule(doc, query)
+            break
     else:
-        yield err
+        yield (False, query)
 
 
 def print_results(results):
